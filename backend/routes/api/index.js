502c6e82247a35e880fcfe7010c5260c75fd6345
Alex Auch <alexsamuelauch@gmail.com>
2022-09-22 22:56:59 -0600

    Added user login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js')
+const usersRouter = require('./users.js')
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js')
 const { User } = require('../../db/models')
 
+//user session restore
 router.use(restoreUser)
+//router for session resource
+router.use('/session', sessionRouter)
+//router for users resource
+router.use('/users', usersRouter)
 
+router.post('/test', (req, res) => {
+    res.json({
+        requestBody: req.body
+    })
+})
 //THE FOLLOWING COMMENTED MIDDLEWARE ARE TESTS:
 
 // router.get('/require-auth', requireAuth, (req, res) => {
@@ -13,11 +25,6 @@ router.use(restoreUser)
 //     return res.json(req.user)
 // })
 
-// router.post('/test', (req, res) => {
-//     res.json({
-//         requestBody: req.body
-//     })
-// })
 
 // router.get('/set-token-cookie', async (req, res) => {
 //     const user = await User.findOne({
