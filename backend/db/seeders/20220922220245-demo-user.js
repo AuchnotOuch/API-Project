440c3d13e0c8836a0884630c53001d24aa97cee5
Alex Auch <alexsamuelauch@gmail.com>
2022-09-26 16:28:32 -0600

    Created seeder data for spots table

--- backend/db/seeders/20220922220245-demo-user.js
+++ backend/db/seeders/20220922220245-demo-user.js
@@ -1,5 +1,6 @@
 'use strict';
 const bcrypt = require('bcryptjs')
+const { Op } = require('express')
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -14,20 +15,40 @@ module.exports = {
     */
     await queryInterface.bulkInsert('Users', [
       {
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
-      },
-      {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password1'),
+        firstName: 'Fake',
+        lastName: 'User1'
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
-      }
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'Fake',
+        lastName: 'User2'
+      },
+      {
+        email: 'user3@user.io',
+        username: 'FakeUser3',
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'Fake',
+        lastName: 'User3'
+      },
+      {
+        email: 'user4@user.io',
+        username: 'FakeUser4',
+        hashedPassword: bcrypt.hashSync('password4'),
+        firstName: 'Fake',
+        lastName: 'User4'
+      },
+      {
+        email: 'user5@user.io',
+        username: 'FakeUser5',
+        hashedPassword: bcrypt.hashSync('password5'),
+        firstName: 'Fake',
+        lastName: 'User5'
+      },
     ])
   },
 
@@ -39,9 +60,7 @@ module.exports = {
      * await queryInterface.bulkDelete('People', null, {});
      */
     await queryInterface.bulkDelete('Users', {
-      username: {
-        [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2']
-      }
+      username: ['FakeUser1', 'FakeUser2', 'FakeUser3', 'FakeUser4', 'FakeUser5']
     })
   }
 };
