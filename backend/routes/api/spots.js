07c352c449c98ca4ff94980c67e94773903f0c1a
Alex Auch <alexsamuelauch@gmail.com>
2022-09-27 22:25:22 -0600

    Added route handler for spots/current

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const { jwtConfig } = require('../../config')
 const { setTokenCookie, requireAuth } = require('../../utils/auth')
 const { User, Spot, Booking, Review, SpotImage, ReviewImage } = require('../../db/models')
 const { check } = require('express-validator')
@@ -6,6 +8,22 @@ const { handleValidationErrors } = require('../../utils/validation')
 
 const router = express.Router()
 
+
+router.get('/current', requireAuth, async (req, res, next) => {
+    // const token = res.cookie('token')
+    // const payload = jwt.decode(token)
+    const ownerId = req.user.id
+
+    const Spots = await Spot.findAll({
+        where: {
+            ownerId: ownerId
+        }
+    })
+    return res.json({ Spots })
+})
+
+
+//get all spots
 router.get('/', async (req, res, next) => {
     const spots = await Spot.findAll()
 
