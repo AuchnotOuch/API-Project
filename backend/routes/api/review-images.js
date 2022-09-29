3dd2485bf8ea0e6ac13a9cc9f3c60158904505b5
Alex Auch <alexsamuelauch@gmail.com>
2022-09-29 15:16:08 -0600

    added router for review images and delete endpoint

---
+++ backend/routes/api/review-images.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const { requireAuth } = require('../../utils/auth')
+const { User, Spot, Booking, Review, SpotImage, ReviewImage } = require('../../db/models')
+
+const router = express.Router()
+
+//delete an existing image for a review
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+
+    const image = await ReviewImage.findByPk(req.params.imageId)
+
+    if (!image) {
+        const err = new Error()
+        err.message = "Review image couldn't be found"
+        err.status = 404
+        res.statusCode = 404
+        return res.json(err)
+    }
+
+    const review = await Review.findByPk(image.reviewId)
+
+    if (req.user.id !== review.userId) {
+        const err = new Error()
+        err.message = "Can't delete an image for a review that doesn't belong to you"
+        err.status = 403
+        res.statusCode = 403
+        return res.json(err)
+    }
+
+    await image.destroy()
+
+    return res.json({
+        message: "Successfully deleted",
+        statusCode: 200
+    })
+})
+
+module.exports = router
