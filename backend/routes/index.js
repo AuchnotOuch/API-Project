e59619de508eb358c89ebeaa7e4e089e730a40ca
Alex Auch <alexsamuelauch@gmail.com>
2022-10-14 21:30:18 -0600

    configured package.json for heroku deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,43 +2,38 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.use('/api', apiRouter)
-//serve react build files in production
+router.use('/api', apiRouter);
+
+// Static routes
+// Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
-    //serve frontend index.html at root route
+    // Serve the frontend's index.html file at the root route
     router.get('/', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken())
-        return res.sendFile(
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-        )
-    })
-    //serve static assets in frontend build folder
-    router.use(express.static(path.resolve('../frontend/build')))
-    //serve frontend index.html at all other routes not starting with /api
+        );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    router.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken())
-        return res.sendFile(
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-        )
-    })
+        );
+    });
 }
-// add XSRF-TOKEN in development
+
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken())
-        return res.json({})
-    })
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        res.status(201).json({});
+    });
 }
-
-router.get('/api/csrf/restore', (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    })
-})
-
-
 
 module.exports = router;
