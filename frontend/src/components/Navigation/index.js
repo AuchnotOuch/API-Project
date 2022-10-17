37a408cd689975561151bb118edd433dca2ec66b
Alex Auch <alexsamuelauch@gmail.com>
2022-10-16 22:05:12 -0600

    added signup modal

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal'
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
@@ -17,7 +18,7 @@ function Navigation({ isLoaded }) {
         sessionLinks = (
             <>
                 <LoginFormModal />
-                <NavLink to="/signup">Sign Up</NavLink>
+                <SignupFormModal />
             </>
         );
     }
@@ -25,7 +26,7 @@ function Navigation({ isLoaded }) {
     return (
         <ul>
             <li>
-                <NavLink exact to="/">Home</NavLink>
+                <NavLink id='home-button' exact to="/">Home</NavLink>
                 {isLoaded && sessionLinks}
             </li>
         </ul>
