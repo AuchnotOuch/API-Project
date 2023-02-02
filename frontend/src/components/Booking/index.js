feaaaeb28c30c54b68ba833c088eebd719000d4b
Alex Auch <alexsamuelauch@gmail.com>
2023-02-02 15:53:57 -0700

    basic styling for booking component

--- frontend/src/components/Booking/index.js
+++ frontend/src/components/Booking/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from 'react-calendar'
 import './Booking.css'
 
@@ -8,11 +8,22 @@ const Booking = ({ spot }) => {
     const date = new Date()
     const [checkin, setCheckin] = useState(date.toLocaleDateString())
     const [checkout, setCheckout] = useState(date.toLocaleDateString())
+    const [dayTotal, setDayTotal] = useState(null)
     const [mountCalendar, setMountCalendar] = useState(false)
 
     const mount = () => {
         setMountCalendar(!mountCalendar)
     }
+
+    useEffect(() => {
+        const days = () => {
+            let start = new Date(checkin)
+            let end = new Date(checkout)
+            let totalDays = (end.getTime() - start.getTime()) / (1000 * 3600 * 24)
+            setDayTotal(totalDays)
+        }
+        days()
+    }, [checkin, checkout])
 
     return (
         <div className='book-spot'>
@@ -36,6 +47,18 @@ const Booking = ({ spot }) => {
                     </div>
                 </div>
             </button>
+            <button className="reserve">Reserve</button>
+            <p>You won't be charged yet.</p>
+            <div className="price">
+                <div className="per-night">${spot.price} X {dayTotal} nights</div>
+                <div className="subtotal">${spot.price * dayTotal}</div>
+            </div>
+            <div className="total">
+                <div className="total-price">
+                    <div>Total before taxes:</div>
+                    <div className="subtotal">${spot.price * dayTotal}</div>
+                </div>
+            </div>
         </div>
     )
 
